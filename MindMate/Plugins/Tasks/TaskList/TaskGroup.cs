12f50316ca172848c39fdf93a862cbca12e287c9
Syed Umar Anis <syed.umar.anis@gmail.com>
2015-03-05 10:45:00 +0400

    Refactor: Correction to last commit, missing TaskGroup file is included

---
+++ MindMate/Plugins/Tasks/TaskList/TaskGroup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MindMate.Plugins.Tasks
+{
+    public class TaskGroup<T> : CollapsiblePanel
+    {
+        public TaskGroup()
+        {
+            TableLayoutPanel table = new TableLayoutPanel();
+
+            this.Controls.Add(table);
+
+            table.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            table.ColumnCount = 1;
+            table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            table.Location = new System.Drawing.Point(0, 30);
+            //table.Name = "tableLayout"  + this.HeaderText.Replace(' ','_');
+            table.RowCount = 0;
+            table.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            table.Size = new System.Drawing.Size(264, 120);
+            //table.TabIndex = 3;  
+        }
+
+        public TableLayoutPanel Table
+        {
+            get { return (TableLayoutPanel)this.Controls[1]; }
+        }
+                
+    }
+}
