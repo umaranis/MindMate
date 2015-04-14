56f71df355add89720fe0de2a171eaf9b07da322
Syed Umar Anis <syed.umar.anis@gmail.com>
2015-04-15 02:40:11 +0400

    Node selection on MouseHover , instead of MouseEnter

--- MindMate/View/MapControls/MapViewPanel.cs
+++ MindMate/View/MapControls/MapViewPanel.cs
@@ -147,16 +147,24 @@ namespace MindMate.View.MapControls
                         NodeMouseEnter(node, e);
                     }
 
-                    NodeMouseEventArgs args = new NodeMouseEventArgs(e);
-                    args.NodePortion = mapView.GetNodeView(node).GetNodeClickPortion(e.Location);
-                    NodeMouseOver(node, args);
+                    //NodeMouseEventArgs args = new NodeMouseEventArgs(e);
+                    //args.NodePortion = mapView.GetNodeView(node).GetNodeClickPortion(e.Location);
+                    //NodeMouseOver(node, args);
                 }
                 else if(mouseOverNode != null)
                 {
                     NodeMouseExit(mouseOverNode, e);
                     mouseOverNode = null;
                 }
-            }
+
+                if (resetHoverEvent)
+                {
+                    const uint HOVER_DEFAULT = 0xFFFFFFFF;
+                    TRACKMOUSEEVENT trackMouseEvent = new TRACKMOUSEEVENT(TMEFlags.TME_HOVER, this.Handle, HOVER_DEFAULT);
+                    TrackMouseEvent(ref trackMouseEvent);
+                    resetHoverEvent = false;
+                }
+            }             
 
             base.OnMouseMove(e);
         }
@@ -171,12 +179,87 @@ namespace MindMate.View.MapControls
 
             if (e.Button == System.Windows.Forms.MouseButtons.Right && mouseOverNode != null)
                 NodeRightClick(mouseOverNode, new NodeMouseEventArgs(e));
-
-                        
-            //base.OnMouseUp(e);            
+              
+          
+            //base.OnMouseUp(e);               
+            
         }
 
-                                   
+
+        #region Mouse Hover Event
+
+        private bool resetHoverEvent = false;
+
+        protected override void OnMouseHover(EventArgs e)
+        {
+            resetHoverEvent = true;
+
+            Point clickPosition = this.PointToClient(Cursor.Position);
+            MapNode node = mapView.GetMapNodeFromPoint(clickPosition);
+            if(node != null)
+            {
+                NodeMouseEventArgs args = new NodeMouseEventArgs(
+                    new MouseEventArgs(System.Windows.Forms.MouseButtons.None, 0, clickPosition.X, clickPosition.Y, 0));
+                NodeMouseOver(mouseOverNode, args);
+            }
+
+            base.OnMouseHover(e);                      
+        }
+
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern int TrackMouseEvent(ref TRACKMOUSEEVENT lpEventTrack);
+
+        [System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential)]
+        public struct TRACKMOUSEEVENT
+        {
+            public Int32 cbSize;    // using Int32 instead of UInt32 is safe here, and this avoids casting the result  of Marshal.SizeOf()
+            [System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.U4)]
+            public TMEFlags dwFlags;
+            public IntPtr hWnd;
+            public UInt32 dwHoverTime;
+
+            public TRACKMOUSEEVENT(TMEFlags dwFlags, IntPtr hWnd, UInt32 dwHoverTime)
+            {
+                this.cbSize = System.Runtime.InteropServices.Marshal.SizeOf(typeof(TRACKMOUSEEVENT));
+                this.dwFlags = dwFlags;
+                this.hWnd = hWnd;
+                this.dwHoverTime = dwHoverTime;
+            }
+        }
+
+        /// <summary>
+        /// The services requested. This member can be a combination of the following values.
+        /// </summary>
+        /// <seealso cref="http://msdn.microsoft.com/en-us/library/ms645604%28v=vs.85%29.aspx"/>
+        [Flags]
+        public enum TMEFlags : uint
+        {
+            /// <summary>
+            /// The caller wants to cancel a prior tracking request. The caller should also specify the type of tracking that it wants to cancel. For example, to cancel hover tracking, the caller must pass the TME_CANCEL and TME_HOVER flags.
+            /// </summary>
+            TME_CANCEL = 0x80000000,
+            /// <summary>
+            /// The caller wants hover notification. Notification is delivered as a WM_MOUSEHOVER message.
+            /// If the caller requests hover tracking while hover tracking is already active, the hover timer will be reset.
+            /// This flag is ignored if the mouse pointer is not over the specified window or area.
+            /// </summary>
+            TME_HOVER = 0x00000001,
+            /// <summary>
+            /// The caller wants leave notification. Notification is delivered as a WM_MOUSELEAVE message. If the mouse is not over the specified window or area, a leave notification is generated immediately and no further tracking is performed.
+            /// </summary>
+            TME_LEAVE = 0x00000002,
+            /// <summary>
+            /// The caller wants hover and leave notification for the nonclient areas. Notification is delivered as WM_NCMOUSEHOVER and WM_NCMOUSELEAVE messages.
+            /// </summary>
+            TME_NONCLIENT = 0x00000010,
+            /// <summary>
+            /// The function fills in the structure instead of treating it as a tracking request. The structure is filled such that had that structure been passed to TrackMouseEvent, it would generate the current tracking. The only anomaly is that the hover time-out returned is always the actual time-out and not HOVER_DEFAULT, if HOVER_DEFAULT was specified during the original TrackMouseEvent request.
+            /// </summary>
+            TME_QUERY = 0x40000000,
+        }
+
+        #endregion Mouse Hover Event
 
     }
 }
