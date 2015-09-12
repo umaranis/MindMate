1fab7dfb3527bf89e8c8a8e9de3f497c48d209f4
umaranis <mail@umaranis.com>
2015-09-12 11:53:47 +0400

    Revert "Revert "Undo/Redo for AttributeSpec add & delete""
    This reverts commit 30a6eba3c5bd61c29bbd02c073b0211b07598f2e.

---
+++ MindMate/Modules/Undo/Changes/AttributeSpecAdd.cs
@@ -0,0 +1,28 @@
+﻿using MindMate.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MindMate.Modules.Undo.Changes
+{
+    class AttributeSpecAdd : IChange
+    {
+        MapTree.AttributeSpec spec;
+
+        public AttributeSpecAdd(MapTree.AttributeSpec spec)
+        {
+            this.spec = spec;
+        }
+
+        public string Description
+        {
+            get { return "Attribute Spec Added"; }
+        }
+
+        public void Undo()
+        {
+            spec.Delete();
+        }
+    }
+}
