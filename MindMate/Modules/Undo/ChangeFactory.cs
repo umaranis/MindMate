4e36a136030c5faa36fa9268e767db9ed2df7024
umaranis <mail@umaranis.com>
2015-09-17 23:26:35 +0400

    Undo/Redo for AttributeSpec ListOption

--- MindMate/Modules/Undo/ChangeFactory.cs
+++ MindMate/Modules/Undo/ChangeFactory.cs
@@ -117,6 +117,8 @@ namespace MindMate.Modules.Undo
                     return new AttributeSpecDataType(spec, (MapTree.AttributeDataType)e.OldValue);
                 case MapTree.AttributeSpecChange.TypeChanged:
                     return new AttributeSpecType(spec, (MapTree.AttributeType)e.OldValue);
+                case MapTree.AttributeSpecChange.ListTypeChanged:
+                    return new AttributeSpecListType(spec, (MapTree.AttributeListOption)e.OldValue);
                 default:
                     return null;
             }
---
+++ MindMate/Modules/Undo/Changes/AttributeSpecListType.cs
@@ -0,0 +1,30 @@
+﻿using MindMate.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MindMate.Modules.Undo.Changes
+{
+    class AttributeSpecListType : IChange
+    {
+        MapTree.AttributeSpec spec;
+        MapTree.AttributeListOption oldValue;
+
+        public AttributeSpecListType(MapTree.AttributeSpec spec, MapTree.AttributeListOption oldValue)
+        {
+            this.spec = spec;
+            this.oldValue = oldValue;
+        }
+
+        public string Description
+        {
+            get { return "Attribute Spec ListOption Change"; }
+        }
+
+        public void Undo()
+        {
+            spec.ListType = oldValue;
+        }
+    }
+}
