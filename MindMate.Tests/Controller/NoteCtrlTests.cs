5289c231bc4f0bfde2968e3541018305d6e0f031
umaranis <mail@umaranis.com>
2016-02-12 14:17:39 +0400

    Unit Tests for NoteEditor and NoteCtrl

---
+++ MindMate.Tests/Controller/NoteCtrlTests.cs
@@ -0,0 +1,346 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MindMate.Controller;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MindMate.Model;
+using MindMate.Serialization;
+using MindMate.View.Dialogs;
+using MindMate.View.NoteEditing;
+
+namespace MindMate.Tests.Controller
+{
+    [TestClass()]
+    public class NoteCtrlTests
+    {
+        [TestMethod()]
+        public void NoteCtrl()
+        {
+            MetaModel.MetaModel.Initialize();
+            var persistence = new PersistenceManager();
+            var nodeEditor = new NoteEditor();
+            var sut = new NoteCtrl(nodeEditor, persistence);
+
+            Assert.IsNotNull(sut);
+        }
+
+        [TestMethod()]
+        private Form CreateForm()
+        {
+            Form form = new Form();
+            form.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            form.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            form.ClientSize = new System.Drawing.Size(415, 304);
+            form.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            form.KeyPreview = true;
+            form.MaximizeBox = false;
+            form.MinimizeBox = false;
+            form.Name = "TestForm";
+            form.ShowInTaskbar = false;
+            form.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            form.Text = "Test Form";
+            form.Size = new Size(320,320);
+            return form;
+        }
+
+        [TestMethod()]
+        public void NoteCtrl_AssignNote_SetRichTextFirst()
+        {
+            MetaModel.MetaModel.Initialize();
+            var persistence = new PersistenceManager();
+            var noteEditor = new NoteEditor();
+
+            bool result = true;
+
+            var form = CreateForm();
+            form.Controls.Add(noteEditor);
+            form.Shown += (sender, args) =>
+            {
+                var tree = persistence.NewTree();
+
+                var sut = new NoteCtrl(noteEditor, persistence);
+
+                //order of below to lines is causing the test to fail
+                tree.Tree.RootNode.RichContentText = "ABC";
+                tree.Tree.RootNode.RichContentType = NodeRichContentType.NOTE;
+                
+
+                result = noteEditor.HTML != null && noteEditor.HTML.Contains("ABC");
+
+                form.Close();
+            };
+            form.ShowDialog();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void NoteCtrl_AssignNote_EditorUpdated()
+        {
+            MetaModel.MetaModel.Initialize();
+            var persistence = new PersistenceManager();
+            var noteEditor = new NoteEditor();
+
+            bool result = true;
+
+            var form = CreateForm();
+            form.Controls.Add(noteEditor);
+            form.Shown += (sender, args) =>
+            {
+                var tree = persistence.NewTree();
+
+                var sut = new NoteCtrl(noteEditor, persistence);
+
+                tree.Tree.RootNode.RichContentType = NodeRichContentType.NOTE;
+                tree.Tree.RootNode.RichContentText = "ABC";
+
+                result = noteEditor.HTML != null && noteEditor.HTML.Contains("ABC");
+
+                form.Close();
+            };
+
+            form.ShowDialog();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void NoteCtrl_AssignNoteToUnselected()
+        {
+            MetaModel.MetaModel.Initialize();
+            var persistence = new PersistenceManager();
+            var noteEditor = new NoteEditor();
+
+            bool result = true;
+
+            var form = CreateForm();
+            form.Controls.Add(noteEditor);
+            form.Shown += (sender, args) =>
+            {
+                var tree = persistence.NewTree();
+                var c1 = new MapNode(tree.Tree.RootNode, "c1");
+
+                var sut = new NoteCtrl(noteEditor, persistence);
+
+                c1.RichContentType = NodeRichContentType.NOTE;
+                c1.RichContentText = "ABC";
+
+                result = noteEditor.HTML == null;
+
+                form.Close();
+            };
+
+            form.ShowDialog();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void NoteCtrl_AssignNoteToUnselected_ClearNoteEditor()
+        {
+            MetaModel.MetaModel.Initialize();
+            var persistence = new PersistenceManager();
+            var noteEditor = new NoteEditor();
+
+            bool result = true;
+
+            var form = CreateForm();
+            form.Controls.Add(noteEditor);
+            form.Shown += (sender, args) =>
+            {
+                var tree = persistence.NewTree();
+                var c1 = new MapNode(tree.Tree.RootNode, "c1");
+                c1.Selected = true;
+
+                var sut = new NoteCtrl(noteEditor, persistence);
+
+                c1.RichContentType = NodeRichContentType.NOTE;
+                c1.RichContentText = "ABC";
+
+                c1.Parent.Selected = true;
+
+                result = noteEditor.HTML == null;
+
+                form.Close();
+            };
+
+            form.ShowDialog();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void NoteCtrl_MultiSelection_ClearNoteEditor()
+        {
+            MetaModel.MetaModel.Initialize();
+            var persistence = new PersistenceManager();
+            var noteEditor = new NoteEditor();
+
+            bool result = true;
+
+            var form = CreateForm();
+            form.Controls.Add(noteEditor);
+            form.Shown += (sender, args) =>
+            {
+                var tree = persistence.NewTree();
+                var c1 = new MapNode(tree.Tree.RootNode, "c1");
+                c1.Selected = true;
+
+                var sut = new NoteCtrl(noteEditor, persistence);
+
+                c1.RichContentType = NodeRichContentType.NOTE;
+                c1.RichContentText = "ABC";
+
+                tree.Tree.SelectedNodes.Add(c1.Parent);
+
+                result = noteEditor.HTML == null;
+
+                form.Close();
+            };
+
+            form.ShowDialog();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void NoteCtrl_ChangeCurrentMapTree()
+        {
+            MetaModel.MetaModel.Initialize();
+            var persistence = new PersistenceManager();
+            var noteEditor = new NoteEditor();
+
+            bool result = true;
+
+            var form = CreateForm();
+            form.Controls.Add(noteEditor);
+            form.Shown += (sender, args) =>
+            {
+                var ptree1 = persistence.NewTree();
+                var c1 = new MapNode(ptree1.Tree.RootNode, "c1");
+                c1.Selected = true;
+
+                var sut = new NoteCtrl(noteEditor, persistence);
+
+                c1.RichContentType = NodeRichContentType.NOTE;
+                c1.RichContentText = "ABC";
+
+                var pTree2 = persistence.NewTree();
+
+                result = noteEditor.HTML == null;
+
+                form.Close();
+            };
+
+            form.ShowDialog();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void UpdateNodeFromEditor_WithoutCalling_MapNotUpdated()
+        {
+            MetaModel.MetaModel.Initialize();
+            var persistence = new PersistenceManager();
+            var noteEditor = new NoteEditor();
+
+            bool result = true;
+
+            var form = CreateForm();
+            form.Controls.Add(noteEditor);
+            form.Shown += (sender, args) =>
+            {
+                var ptree1 = persistence.NewTree();
+                var c1 = new MapNode(ptree1.Tree.RootNode, "c1");
+                c1.Selected = true;
+
+                var sut = new NoteCtrl(noteEditor, persistence);
+
+                c1.RichContentType = NodeRichContentType.NOTE;
+                c1.RichContentText = "ABC";
+
+                noteEditor.HTML = "EFG";
+                
+                result = c1.RichContentText != null && c1.RichContentText.Contains("ABC");
+
+                form.Close();
+            };
+
+            form.ShowDialog();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void UpdateNodeFromEditor()
+        {
+            MetaModel.MetaModel.Initialize();
+            var persistence = new PersistenceManager();
+            var noteEditor = new NoteEditor();
+
+            bool result = true;
+
+            var form = CreateForm();
+            form.Controls.Add(noteEditor);
+            form.Shown += (sender, args) =>
+            {
+                var ptree1 = persistence.NewTree();
+                var c1 = new MapNode(ptree1.Tree.RootNode, "c1");
+                c1.Selected = true;
+
+                var sut = new NoteCtrl(noteEditor, persistence);
+
+                c1.RichContentType = NodeRichContentType.NOTE;
+                c1.RichContentText = "ABC";
+
+                noteEditor.HTML = "EFG";
+                sut.UpdateNodeFromEditor();
+
+                result = c1.RichContentText != null && c1.RichContentText.Contains("ABC");
+
+                form.Close();
+            };
+
+            form.ShowDialog();
+
+            Assert.IsTrue(result);
+
+
+        }
+
+        [TestMethod()]
+        public void SetNoteEditorBackColor()
+        {
+            MetaModel.MetaModel.Initialize();
+            var persistence = new PersistenceManager();
+            var noteEditor = new NoteEditor();
+
+            bool result = true;
+
+            var form = CreateForm();
+            form.Controls.Add(noteEditor);
+            form.Shown += (sender, args) =>
+            {
+                var ptree1 = persistence.NewTree();
+                var c1 = new MapNode(ptree1.Tree.RootNode, "c1");
+                c1.Selected = true;
+
+                var sut = new NoteCtrl(noteEditor, persistence);
+                sut.SetNoteEditorBackColor(Color.Azure);
+                
+                result = noteEditor.BackColor.Equals(Color.Azure);
+
+                form.Close();
+            };
+
+            form.ShowDialog();
+
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ MindMate.Tests/View/NoteEditing/NoteEditorTests.cs
@@ -0,0 +1,223 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MindMate.View.NoteEditing;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MindMate.Tests.View.NoteEditing
+{
+    [TestClass()]
+    public class NoteEditorTests
+    {
+        [TestMethod()]
+        public void NoteEditor_Creation()
+        {
+            var sut = new NoteEditor();
+            Assert.IsNotNull(sut);
+        }
+
+        [TestMethod()]
+        public void NoteEditor_Creation_HTMLIsNull()
+        {
+            var sut = new NoteEditor();
+            bool result = true;
+            var form = CreateForm();
+            form.Controls.Add(sut);
+            form.Shown += (sender, args) =>
+            {
+                result = sut.HTML == null;
+                form.Close();
+            };
+            form.ShowDialog();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void NoteEditor_Creation_IsDirtyFalse()
+        {
+            var sut = new NoteEditor();
+            bool result = true;
+            var form = CreateForm();
+            form.Controls.Add(sut);
+            form.Shown += (sender, args) =>
+            {
+                result = !sut.Dirty;
+                form.Close();
+            };
+            form.ShowDialog();
+
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// Setting HTML property programmatically doesn't make Dirty true. Only changes made through interface will make NoteEditor dirty.
+        /// </summary>
+        [TestMethod()]
+        public void NoteEditor_SetHTML_IsDirtyFalse()
+        {
+            var sut = new NoteEditor();
+            bool result = true;
+            var form = CreateForm();
+            form.Controls.Add(sut);
+            form.Shown += (sender, args) =>
+            {
+                result = !sut.Dirty;
+                form.Close();
+            };
+            form.ShowDialog();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void Clear()
+        {
+            var sut = new NoteEditor();
+            bool result = true;
+            var form = CreateForm();
+            form.Controls.Add(sut);
+            form.Shown += (sender, args) =>
+            {
+                sut.HTML = "Sample Test";
+                sut.Clear();
+                result = sut.HTML == null;
+                form.Close();
+            };
+            form.ShowDialog();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void CanExecuteCommand()
+        {
+            var sut = new NoteEditor();
+            bool result = true;
+            var form = CreateForm();
+            form.Controls.Add(sut);
+            form.Shown += (sender, args) =>
+            {
+                result = sut.CanExecuteCommand(NoteEditorCommand.Bold);
+                form.Close();
+            };
+            form.ShowDialog();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void QueryCommandState()
+        {
+            var sut = new NoteEditor();
+            bool result = true;
+            var form = CreateForm();
+            form.Controls.Add(sut);
+            form.Shown += (sender, args) =>
+            {
+                result = !sut.QueryCommandState(NoteEditorCommand.Bold);
+                form.Close();
+            };
+            form.ShowDialog();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void ExecuteCommand()
+        {
+            var sut = new NoteEditor();
+            bool result = true;
+            var form = CreateForm();
+            form.Controls.Add(sut);
+            form.Shown += (sender, args) =>
+            {
+                sut.ExecuteCommand(NoteEditorCommand.Bold);
+                result = sut.QueryCommandState(NoteEditorCommand.Bold);
+                form.Close();
+            };
+            form.ShowDialog();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod()]
+        public void InsertHyperlink()
+        {
+            var sut = new NoteEditor();
+            bool result = true;
+            var form = CreateForm();
+            form.Controls.Add(sut);
+            form.Shown += (sender, args) =>
+            {
+                sut.HTML = "Website";
+                sut.ExecuteCommand(NoteEditorCommand.SelectAll);
+                sut.InsertHyperlink("umaranis.com");
+                result = sut.HTML != null && sut.HTML.Contains("umar");
+                form.Close();
+            };
+            form.ShowDialog();
+
+            Assert.IsTrue(result);
+        }
+
+        //[TestMethod()]
+        //public void InsertImage()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void Search()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void Cut()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void Paste()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void Copy()
+        //{
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void ClearUndoStack()
+        //{
+        //    Assert.Fail();
+        //}
+
+        [TestMethod()]
+        private Form CreateForm()
+        {
+            Form form = new Form();
+            form.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            form.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            form.ClientSize = new System.Drawing.Size(415, 304);
+            form.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            form.KeyPreview = true;
+            form.MaximizeBox = false;
+            form.MinimizeBox = false;
+            form.Name = "TestForm";
+            form.ShowInTaskbar = false;
+            form.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            form.Text = "Test Form";
+            form.Size = new Size(320, 320);
+            return form;
+        }
+    }
+}
