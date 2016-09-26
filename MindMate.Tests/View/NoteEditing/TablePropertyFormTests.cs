b4db29d256b3fa74249d9db617dc515806f0b256
umaranis <mail@umaranis.com>
2016-09-26 19:12:02 +0400

    Tests for TablePropertyForm

---
+++ MindMate.Tests/View/NoteEditing/TablePropertyFormTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MindMate.View.NoteEditing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindMate.Tests.View
+{
+    [TestClass()]
+    public class TablePropertyFormTests
+    {
+        [TestMethod()]
+        public void TablePropertyForm()
+        {
+            var sut = new TablePropertyForm();
+            sut.TableProperties = new HtmlTableProperty(true);            
+            sut.Show();
+            sut.Close();
+            Assert.IsTrue(string.IsNullOrEmpty(sut.TableProperties.CaptionText));
+        }       
+    }
+}
