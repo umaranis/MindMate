2ff80a95ce927491c2e20c1fa8fb4a325a079a49
umaranis <mail@umaranis.com>
2017-01-18 22:16:00 +0400

    Bump up version to 0.4

--- MindMate.Win7/Properties/AssemblyInfo.cs
+++ MindMate.Win7/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("mail@umaranis.com")]
 [assembly: AssemblyProduct("Mind Mate")]
-[assembly: AssemblyCopyright("Copyright © 2016 Syed Umar Anis")]
+[assembly: AssemblyCopyright("Copyright © 2017 Syed Umar Anis")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.*")]
+[assembly: AssemblyVersion("0.4.*")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
